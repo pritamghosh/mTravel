4b8cefb99bc00cb2130d974e05f74131f703c1b3
pritamghosh <19pritamghosh93@gmail.com>
2020-03-02 01:44:40 +0000

    expense

--- src/app/services/expense.service.ts
+++ src/app/services/expense.service.ts
@@ -4,17 +4,21 @@ import { HttpClient } from "@angular/common/http";
 import { Expense } from "../models/expense.model";
 import { environment } from "src/environments/environment";
 import { Booking } from "../models/booking.model";
+import { LoginService } from "./login.service";
 
 @Injectable({
   providedIn: "root"
 })
 export class ExpenseService {
   travelSubject = new Subject<Booking[]>();
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private loginService: LoginService) {}
 
   getTravelPlans() {
+    let user = this.loginService.getUser();
     return this.http
-      .get<Booking[]>(environment.getAllTravelUrl)
+      .get<Booking[]>(environment.getAllTravelUrl, {
+        params: { email: user.email }
+      })
       .subscribe((resp: Booking[]) => this.travelSubject.next(resp));
   }
 
